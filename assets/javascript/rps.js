f78c836725b34433d56d1e81e64911694d301987
kgodwin88 <kgodwin1988@aol.com>
2018-04-23 23:05:56 -0400

    Set up user interface and started javascript

---
+++ assets/javascript/rps.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    var config = {
+        apiKey: "AIzaSyCukhokI89iIbeFqZdThU4ZFV5EBOJUBPo",
+        authDomain: "rockpaperscissors-58ebd.firebaseapp.com",
+        databaseURL: "https://rockpaperscissors-58ebd.firebaseio.com",
+        projectId: "rockpaperscissors-58ebd",
+        storageBucket: "rockpaperscissors-58ebd.appspot.com",
+        messagingSenderId: "148794319011"
+        };
+    firebase.initializeApp(config);
+    
+    var database = firebase.database();
+
+
+    });
